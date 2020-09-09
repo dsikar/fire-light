7f9d5b03fa6d90f82bc79237791d1b624888ae78
Daniel Sikar <dsikar@gmail.com>
2020-09-09 16:37:28 +0100

    Added model with Conv2D option bias=False

--- Codes/inference.py
+++ Codes/inference.py
@@ -9,7 +9,8 @@ from tensorflow.keras.preprocessing.image import img_to_array
 import numpy as np
 import cv2
 import time
-model=load_model(r'take2.h5')
+#model=load_model(r'take2.h5')
+model=load_model(r'TrainedModels/take2_bias_false.h5')
 
 # image size used when training model
 
@@ -19,12 +20,12 @@ IMG_SIZE = 64
 #FPS:  6.761015271629973
 #Fire Probability:  99.99998807907104
 #Predictions:  [[9.99999881e-01 1.09400766e-07]] # FIRE
-image = cv2.imread('datasets/tmp/fire/20200724_175212_001.jpg')
+# image = cv2.imread('datasets/tmp/fire/20200724_175212_001.jpg')
 #Time taken =  0.14594578742980957
 #FPS:  6.851859293855501
 #Fire Probability:  0.00011978886504948605
 #Predictions:  [[1.1978887e-06 9.9999881e-01]] # NO FIRE
-#image = cv2.imread('datasets/tmp/nofire/20200724_175219_001.jpg')
+image = cv2.imread('datasets/tmp/nofire/20200724_175219_001.jpg')
 
 orig = image.copy()
 
